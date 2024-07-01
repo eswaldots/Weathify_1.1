376124b036587b588e0da79186a3b3bd949d22bf
Aaron Avila <aaronvendedor@gmail.com>
2024-07-01 17:48:09 +0000

    Colored config button

--- Weathify/src/Page.tsx
+++ Weathify/src/Page.tsx
@@ -38,7 +38,7 @@ export default function Page() {
                     <div className={''}>
                         <Weather weatherData={datas}></Weather>
                     </div>
-                    <Button href={'/config/city'} as={Link} isIconOnly id={'cfg'} className={'absolute bg-[#222020] text-[#eb9951] right-0 bottom-0 m-4'}>
+                    <Button href={'/config/city'} as={Link} isIconOnly id={'cfg'} className={'absolute bg-[#32302D] text-[#eb9951] right-0 bottom-0 m-4'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                              stroke="currentColor" className="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
